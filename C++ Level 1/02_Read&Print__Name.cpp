2974359e83a902d1b5d77f20045951b45ba6a31c
Achraf-Kharbouch <ashrafkharbouch@gmail.com>
2026-08-31 18:11:01 +0100

    feat: add user name input and display

---
+++ C++ Level 1/02_Read&Print__Name.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string readName() {
+	string Name;
+	cout << "Please enter your name : ";
+	getline(cin, Name);
+	return Name;
+}
+void printName(string PrintName) {
+
+	cout << "\nYour name is " << PrintName << endl;
+}
+int main()
+{
+	printName(readName());
+}
